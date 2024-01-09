8cd481c52cd50bd97ca7f06aa3af4f79bfbf56c5
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2024-01-09 16:26:08 +0300

    add changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../navbar/navBar.css';
+import './home.css';
 import NavBar from '../navbar/NavBar';
 
 const Home = () => (
 
-  <div>
+  <div className="Homepage">
     <NavBar />
   </div>
 
