c17e67763aeea338d651a28f3f54932a6f4f4959
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2024-01-16 17:06:03 +0300

    add changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,6 +7,15 @@ const Home = () => (
 
   <div className="Homepage">
     <NavBar />
+    <div className="home-container">
+      <div className="home-word">
+        <h2>connect People with Pets</h2>
+        <h4>Learn About out passionate team and how you can help</h4>
+      </div>
+      <div className="home-donate-button">
+        <button type="button">Donate</button>
+      </div>
+    </div>
   </div>
 
 );
