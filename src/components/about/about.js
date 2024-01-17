2ac8add043355b11150413acef4a6264e3f9707d
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2024-01-17 14:08:55 +0300

    additional content

---
+++ src/components/about/about.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './about.css';
+
+
+const About = () => (
+
+  <div className='About-container'>
+    <div className='About-wrapper'>
+        <h1>About Reach A Paw</h1>
+        <div className='card-container'>
+            <div className='card-wrapper'>
+                <div className='card-image'>
+                    
+
+                </div>
+                <h4>
+
+                </h4>
+
+                <p>
+                    
+                </p>
+
+            </div>
+        </div>
+    </div>
+  </div>
+
+);
+
+export default About;
